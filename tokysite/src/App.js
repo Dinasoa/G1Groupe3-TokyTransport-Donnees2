08d376a2baedfa83c7c7c8dd36f479406452bee9
Iloniavo RANDRIAMANGA <hei.iloniavo@gmail.com>
2022-06-23 23:03:28 +0300

    fix : Add two components and update some files

--- tokysite/src/App.js
+++ tokysite/src/App.js
@@ -11,8 +11,8 @@ import LITE from './image/LITEBG.png';
 import CoteGestion from './coteGestion.js';
 import CoteClient from './coteClientt.js';
 import { useState } from 'react';
-
-
+import InputClient from './inputClient';
+import InputGestion from './inputGestion';
 
 function App() {
   const [ state , setState ] = useState(true); 
@@ -21,35 +21,19 @@ function App() {
   } 
   let affiche;
   let label;
+  let inputs;
   if(state){
      affiche = <CoteClient/>;
      label = "Switch to timer";
+     inputs = <InputClient/>
   }
   else {
      affiche = <CoteGestion/>;
      label = "Switch to clock";
+     inputs = <InputGestion/>;
   }
 
-    const actions = [
-      { label: "Antananarivo", value: 1 },
-      { label: "Antsirabe", value: 2 },
-      { label: "Fianarantsoa", value: 3 } ,
-      { label: "Morondava", value: 4 } ,
-      { label: "Tamatave", value: 5 } ,
-      { label: "Mahajanga", value: 6 } ,
-      { label: "Diego", value: 7 }
-    ];
-
-    const offres = [
-      { label: "PREMIUM", value: 1 },
-      { label: "VIP", value: 2 },
-      { label: "LITE", value: 3 }
-    ]
-
-    const jour = [
-      { label: "DAY", value: 1 },
-      { label: "NIGHT", value: 2 }
-    ]
+    
 return (
   <>
   {/* NAVBAR START */}
@@ -122,33 +106,7 @@ return (
     
   <div class="card containers" id="table">
       <div class="card-body">
-        <div className='inputs'>
-          
-            <div>
-            <label className='first'>Date Departure</label>
-              <BootstrapDatePickerComponent/>
-            </div>
-
-            <div>
-            <label>Departure</label>
-            <Select options={ actions } />
-            </div>
-            
-            <div>
-            <label>Arrival</label>
-              <Select options={actions} />
-            </div>
-            
-            <div>
-            <label>Offers</label>
-            <Select options={offres} />
-            </div>
-          
-            <div>
-            <label>Day/Night</label>
-              <Select options={jour} />
-            </div>
-        </div>
+        {inputs}
       </div>
 
       <button className='btn btn-secondary bouton' > Search </button>
