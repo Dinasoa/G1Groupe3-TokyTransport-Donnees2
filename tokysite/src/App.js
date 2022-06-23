9ec4970cc7634c60fb0802778868b575fdf4c594
Dinasoa <hei.dinasoa@gmail.com>
2022-06-23 06:04:58 -0900

    feat : adding folders and files

--- tokysite/src/App.js
+++ tokysite/src/App.js
@@ -28,8 +28,8 @@ function App() {
     ]
 
     const jour = [
-      { label: "JOUR", value: 1 },
-      { label: "NUIT", value: 2 }
+      { label: "DAY", value: 1 },
+      { label: "NIGHT", value: 2 }
     ]
 return (
   <>
@@ -103,103 +103,86 @@ return (
     
   <div class="card containers" id="table">
       <div class="card-body">
-          <div className='inputs'>
-        
-       <div>
-       <label className='first'>Date</label>
-        <BootstrapDatePickerComponent/>
-       </div>
-
-       <div>
-       <label>Depart</label>
-       <Select options={ actions } />
-       </div>
-      
-      <div>
-      <label>Arrivée  </label>
-        <Select options={actions} />
-      </div>
-        
-
-      <div>
-      <label>Offres</label>
-      <Select options={offres} />
-      </div>
-    
-      <div>
-      <label>Nuit/Jour</label>
-        <Select options={jour} />
-      </div>
-          </div>
-        <p className='text-white'>Affcihe les voyages selon les critères ci-dessus </p>
-
-          <div className='row'>
-        <div className='col-md-4'>
-
-        </div>
-
-        <div className='col-md-5'>
-
-
-        <table id="tab">
-      
-      <tr>
-      <th id="numero"> id_voyage </th>
-             
-      <th id="nom">place</th>
-      
-      <th id="prenom">jour_depart</th>
-
-      <th id="prenom">Reserver</th>
-             
-      </tr>
-
-      <tr>
-          <td>1</td>
-          <td>*Affiche les places disponibles pour un voyage </td>
-          <td></td>
-          <td></td>
-
-      </tr>
-
-      
-      <tr>
-          <td>2</td>
-          <td></td>
-          <td></td>
-          <td></td>
-      </tr>
-
-      <tr>
-          <td>3</td>
-          <td></td>
-          <td></td>
-          <td></td>
-
-      </tr>
-
-      <tr>
-          <td>4</td>
-          <td></td>
-          <td></td>
-          <td></td>
-      </tr>
-</table>
-
-        </div>
-
-        <div className='col-md-3'>
-
-        </div>
-
-      </div>
-       
-    
-      </div>
-  </div>
-    
-{/* TABLE ENDING */}
+        <div className='inputs'>
           
+            <div>
+            <label className='first'>Date Departure</label>
+              <BootstrapDatePickerComponent/>
+            </div>
+
+            <div>
+            <label>Departure</label>
+            <Select options={ actions } />
+            </div>
+            
+            <div>
+            <label>Arrival</label>
+              <Select options={actions} />
+            </div>
+            
+            <div>
+            <label>Offers</label>
+            <Select options={offres} />
+            </div>
+          
+            <div>
+            <label>Day/Night</label>
+              <Select options={jour} />
+            </div>
+        </div>
+      </div>
+
+      <button className='btn btn-secondary bouton'> Search </button>
+
+      <table class="table container text-white table-bordered">
+  <thead>
+    <tr>
+      <th scope="col">id_voyage</th>
+      <th scope="col">place</th>
+      <th scope="col">nombre de place</th>
+      <th scope="col">jour_depart</th>
+      <th scope="col">reserver</th>
+
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bookmark-check" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M10.854 5.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
+  <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"/>
+</svg>
+        Afficher les places disponibles</td>
+      <td>
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bookmark-check" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M10.854 5.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
+  <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"/>
+</svg>
+        Afficher les nombres de places displonibles</td>
+      <td>DATE</td>
+      <td>nouvelle reservation</td>  
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Places disponibles</td>
+      <td>nombre</td>
+      <td>DATE</td>
+      <td>nouvelle reservation</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Places disponibles</td>
+      <td>nombre</td>
+      <td>DATE</td>
+      <td>Nouvelle reservation</td>
+    </tr>
+  </tbody>
+</table>
+  </div>  
+
+ 
+  
         </>
         
   );
