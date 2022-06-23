03972c37e1524ac9ee4690c9e7b038a43ebbaf61
Dinasoa <hei.dinasoa@gmail.com>
2022-06-23 03:55:01 -0900

    feat : adding folders and files

---
+++ tokysite/src/App.js
@@ -0,0 +1,208 @@
+import './App.css';
+import logo from './image/logo-bg.png' ;
+import logo2 from './image/logo.png' ;
+import Select from 'react-select';
+import BootstrapDatePickerComponent from './BootstrapDatePickerComponent';
+import InfoModal from './ReserverButton';
+import Reserver from './ReserverButton';
+import VIP from './image/VIPBG.png';
+import PREMIUM from './image/PREMIUMBG.png';
+import LITE from './image/LITEBG.png'
+
+function App() {
+  
+    const actions = [
+      { label: "Antananarivo", value: 1 },
+      { label: "Antsirabe", value: 2 },
+      { label: "Fianarantsoa", value: 3 } ,
+      { label: "Morondava", value: 4 } ,
+      { label: "Tamatave", value: 5 } ,
+      { label: "Mahajanga", value: 6 } ,
+      { label: "Diego", value: 7 }
+    ];
+
+    const offres = [
+      { label: "PREMIUM", value: 1 },
+      { label: "VIP", value: 2 },
+      { label: "LITE", value: 3 }
+    ]
+
+    const jour = [
+      { label: "JOUR", value: 1 },
+      { label: "NUIT", value: 2 }
+    ]
+return (
+  <>
+  {/* NAVBAR START */}
+        <nav class="navbar">
+            <img src={logo} alt=""></img>    
+            <ul>
+                <li>
+                    <a href="">HOME</a>
+                </li>
+                <li>
+                    <a href="#offers">Offers</a>
+                </li>
+                <li>
+                  <a href="#bookTrip">Booking</a>
+                </li>   
+                <li>
+                <a href="#table">Trip</a>
+                </li> 
+            </ul>
+        </nav>
+
+  {/* NAVBAR ENDING */}
+          
+  {/* HOME SART */}
+            
+              <div class="bg-image">
+                <img src={logo2}></img>
+                    <div class="paragraphe">
+                        <h1>TOKYTRANS</h1>
+                        <p>Travel beyond the limit</p>
+                        <Reserver/>
+                    </div>
+              </div>
+
+  {/* HOME ENDING */}
+
+
+  {/* OFFERS START */}
+
+    <div className='offers'>
+      <h1 className="text-center" id="offers"> Available offers </h1>
+
+        <div className="offres text-center">
+              <div class="col">
+                  <img src={VIP}></img>
+                    <h2 class="fw-normal">PREMIUM</h2>
+                    <p>lorem ipsum</p>
+                    <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
+              </div>
+
+              <div class="col">
+                  <img src={LITE}></img>
+                    <p>lorem  ipsum</p>
+                    <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
+              </div>
+
+              <div class="col">
+                  <img src={PREMIUM}></img>
+                    <h2 class="fw-normal">VIP</h2>
+                    <p>lorem ipsum</p>
+                    <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
+              </div>
+
+        </div>
+    </div>
+
+    {/* OFFERS ENDING */}
+
+    {/* TABLE START */}
+    
+  <div class="card containers" id="table">
+      <div class="card-body">
+          <div className='inputs'>
+        
+       <div>
+       <label className='first'>Date</label>
+        <BootstrapDatePickerComponent/>
+       </div>
+
+       <div>
+       <label>Depart</label>
+       <Select options={ actions } />
+       </div>
+      
+      <div>
+      <label>Arrivée  </label>
+        <Select options={actions} />
+      </div>
+        
+
+      <div>
+      <label>Offres</label>
+      <Select options={offres} />
+      </div>
+    
+      <div>
+      <label>Nuit/Jour</label>
+        <Select options={jour} />
+      </div>
+          </div>
+        <p className='text-white'>Affcihe les voyages selon les critères ci-dessus </p>
+
+          <div className='row'>
+        <div className='col-md-4'>
+
+        </div>
+
+        <div className='col-md-5'>
+
+
+        <table id="tab">
+      
+      <tr>
+      <th id="numero"> id_voyage </th>
+             
+      <th id="nom">place</th>
+      
+      <th id="prenom">jour_depart</th>
+
+      <th id="prenom">Reserver</th>
+             
+      </tr>
+
+      <tr>
+          <td>1</td>
+          <td>*Affiche les places disponibles pour un voyage </td>
+          <td></td>
+          <td></td>
+
+      </tr>
+
+      
+      <tr>
+          <td>2</td>
+          <td></td>
+          <td></td>
+          <td></td>
+      </tr>
+
+      <tr>
+          <td>3</td>
+          <td></td>
+          <td></td>
+          <td></td>
+
+      </tr>
+
+      <tr>
+          <td>4</td>
+          <td></td>
+          <td></td>
+          <td></td>
+      </tr>
+</table>
+
+        </div>
+
+        <div className='col-md-3'>
+
+        </div>
+
+      </div>
+       
+    
+      </div>
+  </div>
+    
+{/* TABLE ENDING */}
+          
+        </>
+        
+  );
+}
+
+export default App;
